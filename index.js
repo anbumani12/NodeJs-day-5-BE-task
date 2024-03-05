4702deb4ab1eaf7e0f6e518cccd53d12d603a792
anbumani12 <anbumani15012002@gmail.com>
2024-03-05 17:05:37 +0530

    code

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ const DB_URL = process.env.DB_URL || 'mongodb://localhost:27017/mydatabase';
 
 mongoose
   .connect(DB_URL, {
-    useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
