98fd372866f36c6b46ae927e2e585471f5cf347b
anbumani12 <anbumani15012002@gmail.com>
2024-03-05 19:46:43 +0530

    code

--- index.js
+++ index.js
@@ -6,10 +6,9 @@ import dotenv from "dotenv";
 import cors from "cors";
 import mongoose from "mongoose";
 
-
 dotenv.config();
 const app = express();
-const PORT = Number(process.env.PORT);
+const PORT = process.env.PORT || 3000; // Assuming a default port of 3000
 
 app.use(cors());
 app.use(express.json());
@@ -19,14 +18,16 @@ app.get("/", (req, res) => {
   res.send("Welcome to the Server! 🌐");
 });
 
-DB_URL=mongodb;//localhost:27017
+const DB_URL = process.env.DB_URL || "mongodb://localhost:27017";
+const DB_NAME = process.env.DB_NAME || "mydatabase";
+
 mongoose
-  .connect(`${process.env.DB_URL}/${process.env.DB_NAME}`)
+  .connect(`${DB_URL}/${DB_NAME}`)
   .then(() => {
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
   })
   .catch((error) => {
-    console.log(error);
+    console.error("Error connecting to database:", error);
   });
